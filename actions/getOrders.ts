f9036f601d1d43dfae40dc7759e98063d68e618d
asnet <asnet.code@gmail.com>
2025-02-06 21:47:40 +0100

    Dyplom fix

--- actions/getOrders.ts
+++ actions/getOrders.ts
@@ -3,9 +3,6 @@ import prisma from "@/libs/prismadb";
 export default async function getOrders() {
   try {
     const orders = await prisma.order.findMany({
-      where: {
-        user: { isNot: null }, // Filtrowanie zamówień bez użytkownika
-      },
       include: {
         user: true,
       },
--- actions/getOrdersByUserId.tsx
+++ actions/getOrdersByUserId.tsx
@@ -3,15 +3,14 @@ import prisma from "@/libs/prismadb";
 export default async function getOrdersByUserId(userId: string) {
   try {
     const orders = await prisma.order.findMany({
-      where: {
-        userId: userId,
-        user: { isNot: null }, // Zapewnia, że user istnieje
-      },
       include: {
         user: true,
       },
       orderBy: {
         createDate: "desc",
+      },
+      where: {
+        userId: userId,
       },
     });
 
